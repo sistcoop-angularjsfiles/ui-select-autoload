41a4c2bc77f5d3d7563e135a96dc29be3a4d4741
carlos <carlosthe19916@gmail.com>
2015-04-23 17:10:57 -0500

    first commit

--- dist/select.utils.js
+++ dist/select.utils.js
@@ -4,7 +4,7 @@
 
     var module = angular.module('ui.select.utils', ['ui.select']);
 
-    module.directive('uiAutofocus', function($timeout) {
+    module.directive('uiAutofocus', ['$timeout', function($timeout) {
         return {
             restrict: 'A',
             require: 'uiSelect',
@@ -20,9 +20,9 @@
                 }
             }
         };
-    });
+    }]);
 
-    module.directive('uiSelectAutoload', function(Util) {
+    module.directive('uiSelectAutoload', function() {
         return {
             restrict: 'E',
             require: '^uiSelect',
@@ -32,26 +32,42 @@
                 comparator: '=',
                 ignoreCase: '@'
             },
-            controller: function($scope) {
+            controller: ['$scope', function($scope) {
                 if(angular.isDefined($scope.comparator) && $scope.comparator){
+
+                    var getElementOfArray = function(object, attributeName, toCompare, ignoreCase){
+                        if(object.length){
+                            for(var i = 0; i<object.length; i++){
+                                var attribute1 = angular.isDefined(ignoreCase) ? object[i][attributeName.toString()].toLowerCase() : object[i][attributeName.toString()];
+                                var attribute2 = angular.isDefined(ignoreCase) ? toCompare.toLowerCase() : toCompare;
+                                if(attribute1 == attribute2)
+                                    return object[i];
+                            }
+                        }
+                        return undefined;
+                    };
+
                     var listener = $scope.$parent.$watch('$select.items',function(newValue, oldValue){
                         if(angular.isDefined(newValue) && newValue.length){
                             var items = $scope.$parent.$select.items;
-                            var selected = Util.getElementOfArray(items, $scope.pkName, $scope.comparator, $scope.ignoreCase);
+                            var selected = getElementOfArray(items, $scope.pkName, $scope.comparator, $scope.ignoreCase);
                             $scope.$parent.$select.ngModel.$setViewValue(selected);
                             $scope.$parent.$select.ngModel.$render();
                             //unwatch listener
                             listener();
                         }
                     }, true);
+
                 }
 
                 $scope.$parent.$watch('$select.ngModel.$modelValue', function(newValue, oldValue){
+
                     if(angular.isDefined(newValue)){
                         $scope.comparator = $scope.$parent.$select.ngModel.$modelValue[$scope.pkName];
                     }
+
                 }, true);
-            }
+            }]
         };
     });
 
--- src/select.utils.js
+++ src/select.utils.js
@@ -22,7 +22,7 @@
         };
     });
 
-    module.directive('uiSelectAutoload', function(Util) {
+    module.directive('uiSelectAutoload', function() {
         return {
             restrict: 'E',
             require: '^uiSelect',
@@ -34,22 +34,38 @@
             },
             controller: function($scope) {
                 if(angular.isDefined($scope.comparator) && $scope.comparator){
+
+                    var getElementOfArray = function(object, attributeName, toCompare, ignoreCase){
+                        if(object.length){
+                            for(var i = 0; i<object.length; i++){
+                                var attribute1 = angular.isDefined(ignoreCase) ? object[i][attributeName.toString()].toLowerCase() : object[i][attributeName.toString()];
+                                var attribute2 = angular.isDefined(ignoreCase) ? toCompare.toLowerCase() : toCompare;
+                                if(attribute1 == attribute2)
+                                    return object[i];
+                            }
+                        }
+                        return undefined;
+                    };
+
                     var listener = $scope.$parent.$watch('$select.items',function(newValue, oldValue){
                         if(angular.isDefined(newValue) && newValue.length){
                             var items = $scope.$parent.$select.items;
-                            var selected = Util.getElementOfArray(items, $scope.pkName, $scope.comparator, $scope.ignoreCase);
+                            var selected = getElementOfArray(items, $scope.pkName, $scope.comparator, $scope.ignoreCase);
                             $scope.$parent.$select.ngModel.$setViewValue(selected);
                             $scope.$parent.$select.ngModel.$render();
                             //unwatch listener
                             listener();
                         }
                     }, true);
+
                 }
 
                 $scope.$parent.$watch('$select.ngModel.$modelValue', function(newValue, oldValue){
+
                     if(angular.isDefined(newValue)){
                         $scope.comparator = $scope.$parent.$select.ngModel.$modelValue[$scope.pkName];
                     }
+
                 }, true);
             }
         };
